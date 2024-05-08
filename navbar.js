9d5a1f4644e208597dc209290085f15fbbb98a3a
Salmaa-Elkholy <133521002+Salmaa-Elkholy@users.noreply.github.com>
2024-05-08 21:57:39 +0300

    updated home not last

--- navbar.js
+++ navbar.js
@@ -4,3 +4,10 @@ function changeCss () {
     window.scrollY < 600 ? navElement.style.backgroundColor = 'transparent': navElement.style.color = 'black';
   }
   window.addEventListener("scroll", changeCss , false);
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) {
+        $('nav').addClass('nav-fixed');
+    } else {
+        $('nav').removeClass('nav-fixed');
+    }
+});
