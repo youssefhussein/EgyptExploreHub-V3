c087572bafe3d04885bd954d630cf5e56a0f1dec
youssefhussein <youssefhussein1994@gmail.com>
2024-05-21 23:28:36 +0300

    added users and packages schemas with mongoose , changed styling in addPKG iframe and removed gender other cuz the dr said so 😅😂

---
+++ models/packages.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const packageSchema = Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, min: [1, "Must be above 0"], required: true },
+  description: { type: String, minlength: 20 },
+  startAt: { type: Date },
+  endAt: { type: Date },
+});
+const Package = mongoose.model("package", packageSchema);
---
+++ models/users.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const package = require('./packages')
+const userSchema = Schema({
+  _id: Schema.Types.ObjectId,
+
+  fullname: {
+    type: String,
+    required: true,
+    lowercase: true,
+    minlength: 3,
+   
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    enum: { values: ["Male", "Female"], message: "{VALUE} is not supported" },
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    default: "client",
+  },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }],
+  packagesBought:{
+    type:[package]
+  }
+});
+
+const commentSchema = Schema({
+  author: { type: Schema.Types.ObjectId, ref: "user" },
+  package: package,
+  content: {
+    type: String,
+    required: true,
+    minlength:10,
+  },
+  stars: {
+    type: Number,
+    required: true,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+const Comment = mongoose.model("comment",commentSchema);
+
+module.exports = { User,Comment };
