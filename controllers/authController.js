8bae88f17899792f168de139ce9767608a48d661
youssefhussein <20634930+youssefhussein@users.noreply.github.com>
2024-06-13 03:16:05 +0300

    installed with npm: cookie-parser and jwt .  new ENV variable JWT SECRET . added user signup with encrypted passwords on the db and error checking on the server and sent that error to the client. need @FatmaNageh to style up the errors on the client. will add login and protected routes tommorow

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,53 @@
 const {User} = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 // controller actions
+
+//signup page
 module.exports.signup_get = (req, res) => {
   res.render('./main/signup',{title:"Sign Up"});
-  console.log(req.body);
+  
 }
 
-module.exports.login_get = (req, res) => {
-  res.render('./main/register',{title:"Login"});
-}
-
+//create a new user
 module.exports.signup_post = async (req, res) => {
   const { fullname, email, password,birthday,gender } = req.body;
-
+  
   try {
     const user = await User.create({ fullname,email, password,birthday,gender });
-    res.status(201).json(user);
-  }
-  catch(err) {
-   
-    res.status(400).send(handleErrors(err));
-  }
- 
-}
+    const token = createToken(user);
+    res.cookie("userLogin", token, { httpOnly: true, maxAge:7200000 , sameSite:"strict"   }); //maxAge is 2 hours cuz milliseconds
 
-module.exports.login_post = async (req, res) => {
+    res.status(201).json({user:user._id});
+    }
+    catch(err) {
+      
+      res.status(400).send(handleErrors(err));
+      }
+      
+      }
+     
+      
+//login page
+module.exports.login_get = (req, res) => {
+        res.render('./main/register',{title:"Login"});
+  }
+//login a user
+  module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
   console.log(email, password);
   res.send('user login');
-}
+  }
 
-
+// errors for creating new user
 const handleErrors = (err) => {
 
-let errorsMessages = {fullname:'',email:'', password:'',birthday:'',gender:''};
+let errorsMessages = {errors:{fullname:'',email:'', password:'',birthday:'',gender:''}};
 
 //populate duplication messages
 if(err.code === 11000){
-  errorsMessages.email = "Email already taken";
+  errorsMessages.errors.email = "Email already taken";
   return errorsMessages;
 }
 
@@ -46,9 +55,17 @@ if(err.code === 11000){
 if(err.message.includes("user validation failed:")){
 Object.values(err.errors).forEach(({properties}) => {
  
-    errorsMessages[properties.path ] = properties.message;
+    errorsMessages.errors[properties.path ] = properties.message;
 
 })
-console.log(errorsMessages);
+
 return errorsMessages;
 }}
+
+//create jwt token
+const createToken = (user) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "2h",
+  });
+  return token;
+};
